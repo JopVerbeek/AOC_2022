a737ae22cd9ada7160e46eea07e776dc6ed61d7a
Jop Verbeek <jop.verbeek@nslogin.nl>
2022-12-01 14:19:57 +0100

    new files

--- day_01/aocd01.py
+++ day_01/aocd01.py
@@ -1,6 +1,6 @@
 with open('input.txt') as f:
     lines = [block.split('\n') for block in f.read().split('\n\n') if block]    
-nums = [sum(list(map(int,[num for num in block if num]))) for block in lines]
+nums = [sum([int(num) for num in block if num]) for block in lines]
 sorted_nums = sorted(nums)
 top_3 = sum(sorted_nums[-3:])
 print(max(nums))
